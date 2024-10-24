b7491aa762af489b9af48fdd0cae4ccabb118152
peasantl <samuel.tan99@rocketmail.com>
2024-10-24 20:24:31 +0800

    Change back to port 80

--- master/run.sh
+++ master/run.sh
@@ -13,13 +13,5 @@ echo "Checking Dependancies"
 pip install -r requirements.txt
 echo "Running Server"
 
-if [ "$1" == "--dev" ]; then
-    export APP_MODE=dev
-    # Run Uvicorn with hot reload and more CORS options for development
-    uvicorn app:app --reload --host 0.0.0.0 --port 8001 
-else
-    # Run Uvicorn normally without hot reload for production
-    #sudo /home/peasantl/PC-Manager/master/.venv/bin/uvicorn app:app --host 0.0.0.0 --port 80    
-    uvicorn app:app --host 0.0.0.0 --port 7082
-
-fi
+#sudo /home/peasantl/PC-Manager/master/.venv/bin/uvicorn app:app --host 0.0.0.0 --port 80    
+uvicorn app:app --host 0.0.0.0 --port 80
