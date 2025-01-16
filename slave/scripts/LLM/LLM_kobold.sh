ba5d0f99ae7b7c8bd6b46c5abece48513b1206a0
PeasantL <samuel.tan99@rocketmail.com>
2025-01-16 19:34:49 +0800

    Changed to xterm

--- slave/scripts/LLM/LLM_kobold.sh
+++ slave/scripts/LLM/LLM_kobold.sh
@@ -4,10 +4,12 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 
-/home/peasantl/Documents/TextGen/custom_scripts/kill.sh
+export DISPLAY=:0
+
+#/home/peasantl/Documents/TextGen/custom_scripts/kill.sh
 
 # Run tabbyAPI
-gnome-terminal -- /home/peasantl/Documents/TextGen/koboldcpp/koboldcpp-linux-x64-cuda1210 /home/peasantl/Documents/TextGen/koboldcpp/config.kcpps &
+xterm -e /home/peasantl/Documents/TextGen/koboldcpp/koboldcpp-linux-x64-cuda1210 /home/peasantl/Documents/TextGen/koboldcpp/config.kcpps &
 # Run SillyTavern
 cd /home/peasantl/Documents/TextGen/SillyTavern
-gnome-terminal -- /home/peasantl/Documents/TextGen/SillyTavern/start.sh &
+xterm -e /home/peasantl/Documents/TextGen/SillyTavern/start.sh &
