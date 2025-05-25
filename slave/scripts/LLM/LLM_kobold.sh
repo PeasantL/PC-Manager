931627869498b231c12c9ad7708b31a426677841
PeasantL <samuel.tan99@rocketmail.com>
2025-05-25 12:19:51 +0800

    Remerging divergence

--- slave/scripts/LLM/LLM_kobold.sh
+++ slave/scripts/LLM/LLM_kobold.sh
@@ -3,5 +3,5 @@
 export DISPLAY=:0
 
 # Run kobald
-xterm -e /home/peasantl/Documents/TextGen/koboldcpp/koboldcpp-linux-x64-cuda1210 /home/peasantl/Documents/TextGen/koboldcpp/config.kcpps &
+xterm -e /home/peasantl/Documents/TextGen/koboldcpp/koboldcpp-linux-x64-cuda1210 /home/peasantl/Documents/TextGen/koboldcpp/config.kcpps
 
